afaa5fb8f15aa50225e615b4a5515918cdaab7b8
Kim <thkm.grd@gmail.com>
2025-01-08 14:08:27 +0100

    echo done

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: kgiraud <kgiraud@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 13:28:50 by kgiraud           #+#    #+#             */
-/*   Updated: 2025/01/08 13:39:24 by kgiraud          ###   ########.fr       */
+/*   Updated: 2025/01/08 14:07:31 by kgiraud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,16 @@ void	ft_echo(char **av)
 	opt = 0;
 	if (ft_strncmp(av[i++], "echo", 4) != 0)
 		return ;
-	if (ft_strncmp(av[i], "-n", 4) == 0)
+	if (av[i] && ft_strncmp(av[i], "-n", 4) == 0)
 	{
 		opt = 1;
 		i++;
 	}
 	while (av[i])
+	{
 		ft_printf("%s", av[i++]);
-	if (opt)
+		ft_printf(" ");
+	}
+	if (!opt)
 		ft_printf("\n");
 }
