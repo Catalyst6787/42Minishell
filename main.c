886862be77f070e4b4b868ec84c9f578cbf895bb
lfaure <faurelucien@proton.me>
2025-01-08 16:45:49 +0100

    split is done

--- main.c
+++ main.c
@@ -67,6 +67,20 @@ int get_last_char(char *s, char c)
 	return(printf("get_last_char did not found char, exiting"), exit(0), 0);
 }
 
+int next_char(char *s, char c)
+{
+	int i;
+
+	i = 1;
+	while(s[i])
+	{
+		if (s[i] == c)
+			return(i);
+		i++;
+	}
+	return(i);
+}
+
 int count_chars(char *s, char c)
 {
 	int i;
@@ -95,24 +109,102 @@ char *remove_lone_quotes(char *s)
 char *clean_input(char *s)
 {
 	// expand $
-	s = remove_lone_quotes(s);
-	s = remove_useless_quotes(s);
+	s = remove_lone_quotes(s); // this makes statements such as echo " quotes:' " impossible
+	s = remove_useless_quotes(s); 
 	// split by space except quoted
 	// replace wildcard
 	// remove quotes
 	return(s);
 }
 
+int	end_of_token(char *s)
+{
+	if (isquote(s[0]))
+		return(next_char(s, s[0]));
+	else
+		return(next_char(s, ' '));
+}
+
+char	*token_dup(char *s)
+{
+	int i;
+	int t;
+	char *dup;
+
+	i = 0;
+	t = 0;
+	t = end_of_token(s);
+	dup = malloc(sizeof(char) * t + 1);
+	dup[t] = '\0';
+	while(i < t)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	return(dup);
+}
+
+char	*quoted_token_dup(char *s)
+{
+	int i;
+	int t;
+
+	i = 0;
+	t = 0;
+	char *dup;
+	t = end_of_token(s);
+	dup = malloc(sizeof(char) * t);
+	dup[t - 1] = '\0';
+	while(i < t - 1)
+	{
+		dup[i] = s[i + 1];
+		i++;
+	}
+	return(dup);
+}
+
+char	**split_tokens(char *s)
+{
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	tmp = NULL;
+	while(s[i])
+	{
+		if (s[i] == ' ')
+			i++;
+		else if (isquote(s[i]))
+		{
+			tmp = quoted_token_dup(s + i);
+			printf("%s\n", tmp);
+			free(tmp);
+			i = end_of_token(s + i) + i + 1;
+		}
+		else
+		{
+			tmp = token_dup(s + i);
+			printf("%s\n", tmp);
+			free(tmp);
+			i = end_of_token(s + i) + i;
+		}
+	}
+	return(NULL);
+}
+
 int main(int ac, char **av, char **envp)
 {
 	//char *line;
 
-	if (ac != 2)
-		return(printf("no args given\n"), 0);
+	//if (ac != 2)
+	//	return(printf("no args given\n"), 0);
 	(void)envp;
+	(void)av;
+	(void)ac;
 
-	char *res = clean_input(ft_strdup(av[1]));
-	printf("%s\n", res);
+	char *res = clean_input(ft_strdup(" \"quotes: \" "));
+	printf("%s\n\n", res);
+	split_tokens(res);
 	free(res);
 	/*
 	line = readline(PROMPT);
