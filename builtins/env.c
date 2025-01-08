48e4638d6f7be93513c58488a849efb253219443
Kim <thkm.grd@gmail.com>
2025-01-08 14:29:48 +0100

    minor changes in env.c

--- builtins/env.c
+++ builtins/env.c
@@ -6,13 +6,11 @@
 /*   By: kgiraud <kgiraud@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 14:09:47 by kgiraud           #+#    #+#             */
-/*   Updated: 2025/01/08 14:28:09 by kgiraud          ###   ########.fr       */
+/*   Updated: 2025/01/08 14:29:21 by kgiraud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../mini.h"
-
-void	ft_echo(char **av);
 
 void	ft_env(char **envp)
 {
