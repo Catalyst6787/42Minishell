12a5e567afd29c38c57649a4fc1a78d4c3c8b3c7
Kim <thkm.grd@gmail.com>
2025-01-09 14:53:55 +0100

    jpp

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: kgiraud <kgiraud@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 13:58:49 by kgiraud           #+#    #+#             */
-/*   Updated: 2025/01/09 13:58:53 by kgiraud          ###   ########.fr       */
+/*   Updated: 2025/01/09 14:53:13 by kgiraud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,6 @@
 
 void	ft_cd(char **av, char **envp)
 {
-	char	*oldpwd;
-	char	*path;
-	
-	oldpwd = getcwd(NULL, 0);
-	if (!oldpwd)
-	{
-		perror("minishell: set old_pwd error");
-		return ;
-	}
-	if (!av[1])
-	{
-		path = getenv("HOME");
-	}
-	else if (ft_strncmp(av[1], "-", 1))
 	// Garder le répertoire courant dans $OLDPWD avec getcwd()
 	// Si échec de getcwd, afficher une erreur et quitter
 
