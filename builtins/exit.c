26a604afd58f52f691884d1b10a343f422ce3e53
Kim <thkm.grd@gmail.com>
2025-02-12 16:08:43 +0100

    exit is clean

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: kgiraud <kgiraud@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 15:28:00 by kgiraud           #+#    #+#             */
-/*   Updated: 2025/02/11 13:02:52 by kgiraud          ###   ########.fr       */
+/*   Updated: 2025/02/12 16:06:22 by kgiraud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,37 @@ int	fgv_exit_arg(int new)
 	return (value);
 }
 
+int	is_all_digits(char *s)
+{
+	if (!s)
+		return (-1);
+	if (*s == '+')
+		s++;
+	while (*s)
+	{
+		if (!ft_isdigit(*s))
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
 int	ft_exit(char **av)
 {
 	if (av[1] && av[2])
-		return (printf("minishell: exit: too many arguments\n"), 1);
-	if (av[1])
-		fgv_exit_arg(ft_atoi(av[1]));
+	{
+		fgv_exit_arg(1);
+		printf("minishell: exit: too many arguments\n");
+	}
+	else if (av[1])
+	{
+		if (is_all_digits(av[1]))
+			fgv_exit_arg(ft_atoi(av[1]));
+		else
+		{
+			printf("minishell: exit: int expected\n");
+			fgv_exit_arg(2);
+		}
+	}
 	return (0);
 }
